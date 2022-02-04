10b1b50db79e7594af3756efd06792780d804f28
Evgeniya <z.dzembas@gmail.com>
2022-02-05 00:00:47 +0300

    work whith customer (reducer, add, remove function). refactor (added constants and actions to the reducer)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addCashAction, getCashAction } from './store/cashReducer';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
-    dispatch({type:"ADD_CASH", payload: cash})
+    dispatch(addCashAction(cash));
   }
 
   const getCash = (cash) => {
-    dispatch({type:"GET_CASH", payload: cash})
+    dispatch(getCashAction(cash));
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    }
+    dispatch(addCustomerAction(customer))
+  }
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
     <div className="App">
-      <div className='cashDiv'>{cash}</div>
+      <div>Balance {cash}</div>
       <div className='buttonsDiv'>
         <button onClick={() => addCash(Number(prompt()))}>Top up your account</button>
         <button onClick={() => getCash(Number(prompt()))}>Withdraw money from the account</button>
+        <button onClick={() => addCustomer(prompt())}>Add customer</button>
       </div>
+      {customers.length > 0 ?
+        <div>
+          {customers.map(customer =>
+            <div onClick={() => removeCustomer(customer)} className='customerName'>{customer.name}</div>
+          )}
+
+        </div>
+        : <div>No clients</div>
+      }
+
     </div>
   );
 }
