88b633e47a722cdec3bd55b71589ff42054134eb
Evgeniya <z.dzembas@gmail.com>
2022-02-05 19:08:37 +0300

    async action using redux-thunk (and jsonplaceholder for async query to datadase)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { fetchCustomers } from './asyncActions/customers';
 import { addCashAction, getCashAction } from './store/cashReducer';
 import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
@@ -34,13 +35,13 @@ function App() {
         <button onClick={() => addCash(Number(prompt()))}>Top up your account</button>
         <button onClick={() => getCash(Number(prompt()))}>Withdraw money from the account</button>
         <button onClick={() => addCustomer(prompt())}>Add customer</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Get all customers from database</button>
       </div>
       {customers.length > 0 ?
         <div>
           {customers.map(customer =>
             <div onClick={() => removeCustomer(customer)} className='customerName'>{customer.name}</div>
           )}
-
         </div>
         : <div>No clients</div>
       }
